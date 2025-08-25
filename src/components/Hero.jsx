487849b02a8d6c14eb057e19f886de9b8ac89a57
SAKTHIVEL <sakthii3011@gmail.com>
2025-08-25 11:03:13 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -82,7 +82,7 @@ const Hero = () => {
 
   const handleEventClick = () => {
     // Navigate to events page
-    navigate('/Events');
+    navigate('/Event');
   };
 
   // Preload all images in the background
